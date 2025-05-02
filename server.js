41603a44cc073e54cf75c73f75b6441420120691
i am 3 <6307500891iam3@gmail.com>
2025-05-02 12:33:20 +0530

    Initial commit

--- server.js
+++ server.js
@@ -41,3 +41,7 @@ app.post("/signup", async (req, res) => {
   await newUser.save();
   res.send("Signup successful!");
 });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
